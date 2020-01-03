dadb9033ce682312ff908f6e4488d87a8a798ee1
Benjamin Candy <bcandyman93@gmail.com>
2020-01-02 20:57:07 -0800

    Added code to insert initial burgers into the burgers table within the burgers_db.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Insert inital burgers into the db
+INSERT INTO burgers_db.burgers (name, devoured)
+VALUES ("Bleu Cheese Bacon Burger", FALSE);
+
+INSERT INTO burgers_db.burgers (name, devoured)
+VALUES ("Mushroom Swiss Burger", FALSE);
+
+INSERT INTO burgers_db.burgers (name, devoured)
+VALUES ("Kobe Burger", FALSE);
