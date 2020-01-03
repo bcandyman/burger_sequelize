ee1edf1872a3b8b280b6813359013af43f47736f
Benjamin Candy <bcandyman93@gmail.com>
2020-01-02 23:54:07 -0800

    added code to console log information from database.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const burger = require('../models/burger');
 
 router.get('/', function (req, res, ) {
-    res.render('index');
+    burger.all(function (data) {
+        console.log(data);
+        res.render('index');
+    })
 })
 
 module.exports = router;
