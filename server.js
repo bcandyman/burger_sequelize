a4708c48af8377402be614cdfb16f464a8e7a0c0
Benjamin Candy <bcandyman93@gmail.com>
2020-01-02 21:19:23 -0800

    Added supporting code for express-handlebars.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT + ' http://localhost:' + PORT);
