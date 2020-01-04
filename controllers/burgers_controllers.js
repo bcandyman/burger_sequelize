7ed37102d75c20c97b3738d379a0cf0e4e8bcfe5
Benjamin Candy <bcandyman93@gmail.com>
2020-01-03 20:40:43 -0800

    Added a post route for testing

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const burger = require('../models/burger');
 
-router.get('/', function (req, res, ) {
-    burger.all(function (data) {
-        console.log(data);
-        res.render('index');
+
+router.route('/')
+    .get(function (req, res) {
+        burger.all(function (data) {
+            console.log(data);
+            return res.render('index');
+        })
     })
-})
+    .post(function (req, res) {
+        console.log(req.body)
+        return res.send(req.body);
+    })
 
 module.exports = router;
