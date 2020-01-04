9bf0681946b601fa35873cedeaed79c32514a35d
Benjamin Candy <bcandyman93@gmail.com>
2020-01-03 20:41:55 -0800

    Added app.use commands.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const exphbs = require('express-handlebars');
 const routes = require('./controllers/burgers_controllers');
+
+const PORT = process.env.PORT || 3000;
+
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
+
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT + ' http://localhost:' + PORT);
