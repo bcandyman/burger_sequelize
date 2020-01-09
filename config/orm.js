710dbe9c9f4fd5d7ddcd3fdc1bd7719d1b1c22cd
Benjamin Candy <bcandyman93@gmail.com>
2020-01-08 23:20:19 -0800

    Added update method to orm object.

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 const connection = require('./connection');
 
 orm = {
+    insert: function (burgerName, table, callback) {
+        const test = { name: burgerName, devoured: false };
+        const queryStr = `INSERT INTO ${table} SET ?`
+        connection.query(queryStr, test, function (err, results) {
+            if (err) {
+                throw err;
+            };
+            callback(results);
+        });
+    },
     selectAll: function (table, callback) {
         const queryStr = `SELECT * FROM ${table}`;
         connection.query(queryStr, function (err, results) {
@@ -19,10 +29,9 @@ orm = {
             callback(results);
         });
     },
-    insert: function (burgerName, table, callback) {
-        const test = { name: burgerName, devoured: false };
-        const queryStr = `INSERT INTO ${table} SET ?`
-        connection.query(queryStr, test, function (err, results) {
+    update: function (table, updateWhat, updateWhereCol, updateWhereVal, updateTo, callback) {
+        const queryStr = `UPDATE ${table} SET ${updateWhat} = ${updateTo} WHERE (${updateWhereCol} = ${updateWhereVal})`;
+        connection.query(queryStr, function (err, results) {
             if (err) {
                 throw err;
             };
