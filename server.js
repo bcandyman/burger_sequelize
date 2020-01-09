ac2e4a16a6062e95b3636cd665738de7e8716c99
Benjamin Candy <bcandyman93@gmail.com>
2020-01-08 23:19:20 -0800

    Deleted empty rows.

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const routes = require('./controllers/burgers_controllers');
 
 const PORT = process.env.PORT || 3000;
 
-
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT + ' http://localhost:' + PORT);
