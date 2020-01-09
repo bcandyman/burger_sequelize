ffb9529808fe282f79a4f651b8c6aac4f26efa25
Benjamin Candy <bcandyman93@gmail.com>
2020-01-08 23:25:52 -0800

    Changed url on post request attached to '#addBurger'. Added event listener to 'button.devour'.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,25 @@
+//-------------------------------------//
+//           EVENT LISTENERS           //
+//-------------------------------------//
+
 $('#addBurger').click(() => {
     const burgerName = $('#burgerName').val()
     $.ajax({
-        url: '/',
+        url: '/api/burger/create',
         type: 'POST',
         data: { name: burgerName }
-    })
+    }).done((res, rej) => {
+        window.location = res.redirect;
+    });
+})
+
+$("button.devour").click(() => {
+    const burgerId = $(this).attr('data-burger-id');
+    $.ajax({
+        url: '/api/burger/devour',
+        type: 'PUT',
+        data: { id: burgerId }
+    }).done((res, rej) => {
+        window.location = res.redirect;
+    });
 });
