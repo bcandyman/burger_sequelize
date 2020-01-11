12a26182708b8c9ede951e0add7615a95746a651
Benjamin Candy <bcandyman93@gmail.com>
2020-01-10 22:05:28 -0800

    Added data validation before creating post request. Added comments.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,19 +2,58 @@
 //           EVENT LISTENERS           //
 //-------------------------------------//
 
-$('#addBurger').click(() => {
-    const burgerName = $('#burgerName').val()
-    $.ajax({
-        url: '/api/burger/create',
-        type: 'POST',
-        data: { name: burgerName }
-    }).done((res, rej) => {
-        window.location = res.redirect;
-    });
-})
 
+// Sends the request to the api route
+function createNewBurger() {
+
+    // record name of the burger to be created.
+    const burgerName = $('#burgerName').val().trim();
+
+    // if burgerName string is not empty, make ajax post request.
+    if (burgerName.length !== 0) {
+        $.ajax({
+            url: '/api/burger/create',
+            type: 'POST',
+            data: { name: burgerName }
+        }).done((res, rej) => {
+
+            // redirect browser to returned route.
+            window.location = res.redirect;
+        });
+    };
+};
+
+
+
+
+//-------------------------------------//
+//           EVENT LISTENERS           //
+//-------------------------------------//
+
+// text input field of burger to be created.
+$('#burgerName').keypress(function (event) {
+
+    // record keyCode value of user pressed key.
+    const keyCode = event.keyCode;
+
+    // if keyCode = 13 (keyCode 13 = Return key).
+    if (keyCode === 13) {
+        createNewBurger();
+    };
+});
+
+// add burger button.
+$('#addBurger').click(function () {
+    createNewBurger();
+});
+
+// devour burger button.
 $("button.devour").click(function () {
+
+    // record name of the burger to be updated.
     const burgerId = $(this).attr('data-burger-id');
+
+    // make ajax put request to update field that defines if burger has been devoured.
     $.ajax({
         url: '/api/burger/devour',
         type: 'PUT',
