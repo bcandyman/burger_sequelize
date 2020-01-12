f82834da9341cc70fa580a75e83795e14968a834
Benjamin Candy <bcandyman93@gmail.com>
2020-01-11 21:12:16 -0800

    Converted file to create model for sequelize.

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,17 @@
-const orm = require('../config/orm');
-
-var burger = {
-    all: function (callback) {
-        orm.selectAll('burgers', function (res) {
-            callback(res);
-        });
-    },
-    allWhere: function (callback) {
-        orm.selectAllWhere('burgers', 'devoured', 0, function (res) {
-            callback(res);
-        });
-    },
-    devour: function (burgerId, callback) {
-        orm.update('burgers', 'devoured', 'id', burgerId, 1, function (res) {
-            callback(res);
-        });
-    },
-    insert: function (burgerName, callback) {
-        orm.insert(burgerName, 'burgers', function (res) {
-            callback(res);
-        });
-    },
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+    return Burger;
 };
-
-module.exports = burger;
