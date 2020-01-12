a4bb80108a47e6cdffe95459fac4bda1ceab6a4a
Benjamin Candy <bcandyman93@gmail.com>
2020-01-11 21:13:10 -0800

    Deleted file on sequelize update.

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
--- Insert inital burgers into the db
-INSERT INTO burgers_db.burgers (name, devoured)
-VALUES ("Bleu Cheese Bacon Burger", FALSE);
-
-INSERT INTO burgers_db.burgers (name, devoured)
-VALUES ("Mushroom Swiss Burger", FALSE);
-
-INSERT INTO burgers_db.burgers (name, devoured)
-VALUES ("Kobe Burger", FALSE);
